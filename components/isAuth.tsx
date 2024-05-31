b1cb2d79eafbbdb07f8a8d4d48aaafea91e44797
Karam Shaarawy <karam.shaarawy@gmail.com>
2024-05-31 23:21:55 +0300

    HOC

---
+++ components/isAuth.tsx
@@ -0,0 +1,22 @@
+"use client";
+// import { getCurrentUserData } from "@/app/utils/currentUser";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function isAuth(Component: any) {
+  // eslint-disable-next-line react/display-name
+  return (props: any) => {
+    const router = useRouter();
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        const isAuth = localStorage.getItem("currentUser");
+        if (isAuth !== null) {
+        } else {
+          redirect("/dashboard");
+        }
+      }
+    }, [router]);
+
+    return <Component {...props} />;
+  };
+}
